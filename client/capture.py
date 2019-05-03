afbcb818bfc4222e0b6cfc44fdf92cab8f357aa2
KIMATA Tetsuya <kimata@green-rabbit.net>
2019-05-03 21:11:34 +0900

    fix: ファイルのコピー方法を paramiko から OpenSSH に変更

--- client/capture.py
+++ client/capture.py
@@ -3,27 +3,74 @@
 
 # Raspberry Pi のカメラで撮影を行い，サーバーにアップロードします
 
-import os
-import paramiko
-import scp
-from picamera import PiCamera
 from datetime import datetime
 
-TMP_FILE_PATH = '/run/user/{}/camera.jpg'.format(os.getuid())
+print('{} SCRIPT Start.'.format(datetime.now()), flush=True)
+
+import os
+import time
+import subprocess
+
+# print('{} Loading paramiko...'.format(datetime.now()), flush=True)
+# import paramiko
+
+print('{} Loading PiCamera...'.format(datetime.now()), flush=True)
+from picamera import PiCamera
+
+TMP_FILE_PATH = '/dev/shm/camera_uid_{}.jpg'.format(os.getuid())
 IMAGE_NAME_FORMAT = 'camera-%Y%m%d_%H%M.jpg'
 
 SSH_HOST = '192.168.2.20'
 SSH_USER = 'guest'
 SSH_KEY  = '/home/pi/.ssh/guest.id_rsa'
 
-def upload(from_file_path, to_file_path):
-    with paramiko.SSHClient() as ssh:
-        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        ssh.connect(hostname=SSH_HOST, port=22,
-                    username=SSH_USER,
-                    pkey=paramiko.RSAKey.from_private_key_file(SSH_KEY))
+SSH_CMD  = 'ssh -i {key} -o IdentitiesOnly=yes -o StrictHostKeyChecking=no {user}@{host}'
+SCP_CMD  = 'scp -i {key} -o IdentitiesOnly=yes -o StrictHostKeyChecking=no -q {from_file_path} {user}@{host}:{to_file_path}'
 
-        scp.SCPClient(ssh.get_transport()).put(from_file_path, to_file_path)
+# NOTE: paramiko は重い(Raspberry Pi Zero W だと import に 10秒近くかかる)ので使わない
+# def upload(from_file_path):
+#     while True:
+#         try:
+#             with paramiko.SSHClient() as ssh:
+#                 ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+#                 ssh.connect(hostname=SSH_HOST, port=22,
+#                             username=SSH_USER,
+#                             pkey=paramiko.RSAKey.from_private_key_file(SSH_KEY))
+
+#                 # NOTE: この時点で時刻が NTP で同期されていない可能性
+#                 # があるので，リモートで date を使ってファイル名を生成する
+#                 ssh_stdout = ssh.exec_command('date "+%Y%m%d_%H%M"')[1]
+#                 to_file_path = 'camera-{}.jpg'.format(ssh_stdout.read().decode().strip())
+
+#                 ftp_client = ssh.open_sftp()
+#                 ftp_client.put(from_file_path, to_file_path)
+#                 ftp_client.close()
+#             break
+#         except:
+#             pass
+#         time.sleep(1)
+
+def upload(from_file_path):
+    while True:
+        try:
+            proc = subprocess.run(
+                [ *(SSH_CMD.format(host=SSH_HOST,user=SSH_USER,key=SSH_KEY).split(' ')),
+                  'date "+%Y%m%d_%H%M"' ],
+                stdout = subprocess.PIPE,
+                check = True
+            )
+            to_file_path = 'camera-{}.jpg'.format(proc.stdout.decode('utf-8').strip())
+
+            subprocess.run(
+                SCP_CMD.format(host=SSH_HOST,user=SSH_USER,key=SSH_KEY,
+                               from_file_path=from_file_path,to_file_path=to_file_path).split(' '),
+                check = True
+            )
+            break
+        except:
+            pass
+        time.sleep(1)
+
 
 def capture():
     image_path = TMP_FILE_PATH
@@ -35,5 +82,9 @@ def capture():
 
     return image_path
 
+
+print('{} START capture...'.format(datetime.now()), flush=True)
 image_path = capture()
-upload(image_path, datetime.now().strftime(IMAGE_NAME_FORMAT))
+print('{} START upload...'.format(datetime.now()), flush=True)
+upload(image_path)
+print('{} FINISH.'.format(datetime.now()), flush=True)
